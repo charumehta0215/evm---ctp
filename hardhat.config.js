86c5c91855c6d14b0912d51e6579e57f20f1bb35
charumehta0215 <mehtacharu0215@gmail.com>
2024-12-13 15:53:11 +0530

    dd

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.0", 
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337
+    }
+  }
+};
